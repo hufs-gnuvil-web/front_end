9891577f46669c86e21790c7ff92eaf3000e5326
yeeun426 <thsudkcla7@naver.com>
2023-05-09 23:55:30 +0900

    feat : 주소 검색 기능 구현중 (react-daum-postcode)

---
+++ src/components/PostAddress.js
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from "react";
+import DaumPostcode from "react-daum-postcode";
+
+const Post = (props) => {
+    const complete = (data) =>{
+        let fullAddress = data.address;
+        let extraAddress = '';
+
+        if (data.addressType === 'R') {
+            if (data.bname !== '') {
+                extraAddress += data.bname;
+            }
+            if (data.buildingName !== '') {
+                extraAddress += (extraAddress !== '' ? `, ${data.buildingName}` : data.buildingName);
+            }
+            fullAddress += (extraAddress !== '' ? ` (${extraAddress})` : '');
+        }
+        console.log(data)
+        console.log(fullAddress)
+        console.log(data.zonecode)
+
+        props.setcompany({
+            ...props.company,
+            address:fullAddress,
+        })
+    }
+
+    const postCodeStyle = {
+        background : "rgba(0,0,0,0.25)",
+        position: "fixed",
+        top: 0,
+        width: "50%",
+        height: "70%",
+        zIndex: 100, 
+    };
+
+  return (
+    <>
+        <DaumPostcode
+          style={postCodeStyle}
+          autoClose
+          onComplete={complete}
+        />
+    </>
+  );
+};
+
+export default Post;
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,17 +1,31 @@
 import React, { useState } from 'react';
 
 import BasicHeader from "../components/BasicHeader";
+import PostAddress from "../components/PostAddress";
+
 import "../styles/SignupStyled.css"
 import "../styles/LoginStyled.css"
-import DaumPostcode from 'react-daum-postcode';
 
 import { Link } from "react-router-dom";
 
 export default function Signup() {
-    const [inputValue, setInputValue] = useState('');
+    const [email, setEmail] = useState('');
     const onChangeInput = (e) => {
-      setInputValue(e.target.value);
+      setEmail(e.target.value);
     };
+
+    // 주소 검색 팝업
+    const [popup, setPopup] = useState(false);
+    const [enroll_company, setEnroll_company] = useState({
+        address:'',
+    });
+    
+    const handleInput = (e) => {
+        setEnroll_company({
+            ...enroll_company,
+            [e.target.name]:e.target.value,
+        })
+    }
 
     return(
         <div className = "Signup">
@@ -36,7 +50,7 @@ export default function Signup() {
                 <div className = "signup-items">
                     <div className = "signup-items-txt">사용자 비밀번호를 입력해주세요.</div>
                     <input 
-                        type="text"
+                        type="password"
                         placeholder="영문, 숫자, 특수문자를 조합하여 6-13자로 작성해주세요."
                     />
                 </div>
@@ -44,7 +58,7 @@ export default function Signup() {
                 <div className = "signup-items">
                     <div className = "signup-items-txt">비밀번호 확인</div>
                     <input 
-                        type="text"
+                        type="password"
                     />
                 </div>
 
@@ -63,9 +77,9 @@ export default function Signup() {
                             type="text"
                         />
                         @
-                        <input type="text" value={inputValue}/>
+                        <input type="text" value={email}/>
 
-                        <select value = {inputValue} onChange={onChangeInput}>
+                        <select value = {email} onChange={onChangeInput}>
                             <option value="">직접 입력</option>
                             <option value="naver.com">naver.com</option>
                             <option value="gmail.com">gmail.com</option>
@@ -87,9 +101,14 @@ export default function Signup() {
                     <div className = "signup-address">
                         <input 
                             type="text"
+                            required={true} name="address" onChange={handleInput} value={enroll_company.address}
                         />
-                        <button>주소 검색</button>
+                        <button onClick={()=>{
+                            setPopup(!popup)
+                        }}>주소 검색</button>
                     </div>
+                        {popup && <PostAddress company={enroll_company} setcompany={setEnroll_company}></PostAddress>
+                    }
                     <input
                         type = "text"
                     />
